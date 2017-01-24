a68cd9b5d43ee0158e755673bda87891506ef94f
h8324 <h8324@MEDI-426-WS-20>
2017-01-24 19:24:32 +0200

    Initial commit.

---
+++ Olio-Teht-v-3/Olio-Teht-v-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3;
+            Console.Write("Anna ensimmäinen luku  > " );
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna toinen luku > ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna kolmas luku > ");
+            luku3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(luku1 + luku2 + luku3); //Laskee ja tulostaa summan
+            Console.WriteLine((luku1 + luku2 + luku3) / 3); //Laskee kolmen muuttujan keskiarvon
+        }
+    }
+}
